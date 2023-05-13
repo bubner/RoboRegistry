54009175c6cc99709d9ec8e15ea9d14475248cf8
Lucas Bubner <lkbubner@proton.me>
2023-05-13 13:56:16 +0930

    Added base structure to application

--- src/app.py
+++ src/app.py
@@ -3,9 +3,11 @@
     @author: Lucas Bubner, 2023
 """
 
+import firebase
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("auth/login.html")
